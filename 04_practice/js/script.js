7216147e2cb898debbbfa4b65429715878591f9a
AluvarD <antipov_ilya26@mail.ru>
2019-11-03 19:27:13 +0300

    pracrice 4

---
+++ 04_practice/js/script.js
@@ -0,0 +1,25 @@
+window.addEventListener('load',function(){
+	const button = document.querySelector('button');
+	const popup = document.querySelector('.popup');
+	const closepopup = document.querySelector('.closePopup');
+	const menubuttonopen = document.querySelector('i');
+	const menubuttonclose = document.querySelector('.closeMenu');
+	const menu = document.querySelector('.menu');
+
+
+	button.onclick = function(){
+		popup.style.display = 'block';
+	}
+
+	closepopup.onclick = function(){
+		popup.style.display = 'none';
+	}
+
+	menubuttonopen.onclick = function(){
+			menu.style.left = 0;
+	}
+
+	menubuttonclose.onclick = function(){
+		menu.style.left = '-50vw';
+	}
+})
