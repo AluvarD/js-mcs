6cec2fdb300c5388acc852c091349a140675c6a6
AluvarD <antipov_ilya26@mail.ru>
2019-11-11 21:24:51 +0300

    6_4_1

--- 6_4_1_practice/js/script.js
+++ 6_4_1_practice/js/script.js
@@ -1,16 +1,21 @@
 const APIKey = ' '; //не дает залогиниться сайт
 const city = prompt('Введите город:');
 const url = 'http://api.openweathermap.org/data/2.5/forecast/wheather?q='+ city +'&appid='+APIKey;
+const name1 = document.querySelector('.tmp');
+const button = document.querySelector('button');
 
 let xnr = new XMLHttpRequest();
 
 xnr.open('GET', url, false);
-xnr.send();
 
-if(xnr.status != 200){
-	console.log(xnr.status + ' ' + xnr.statusText);
-} else {
-	var DATA = JSON.parse(xnr.responseText);
-	console.log(DATA);
-	document.write(DATA.main.temp - 273);
-}
+button.onclick = function(){
+		xnr.send();
+		if(xnr.status != 200){
+			console.log(xnr.status + ' ' + xnr.statusText);
+		} else {
+			var DATA = JSON.parse(xnr.responseText);
+			console.log(DATA);
+			name1.innerHTML = DATA.main.temp - 273;
+			//document.write(DATA.main.temp - 273);
+		}
+	}
